31596cbf753f827cda7b9d23e967abb2f99c4ff0
Dinar <dinar-97@mail.ru>
2024-01-30 12:29:20 +0300

    added tests

--- src/test/java/page/ProfilePage.java
+++ src/test/java/page/ProfilePage.java
@@ -5,6 +5,7 @@ import data.TestData;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
 
 public class ProfilePage {
     TestData uData = new TestData();
@@ -28,6 +29,11 @@ public class ProfilePage {
     }
     public ProfilePage setPasswordAuth(){
         passwordLoginField.setValue(uData.getPassword()).pressEnter();
+        return this;
+    }
+    public ProfilePage disableBanner() {
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
         return this;
     }
 }
