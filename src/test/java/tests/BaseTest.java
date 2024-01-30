47043a17d78ff2db3279b20fb8a4e6a03843259a
Dinar <dinar-97@mail.ru>
2024-01-30 11:39:19 +0300

    added test api with ui

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -17,11 +17,9 @@ public class BaseTest {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.pageLoadStrategy = "eager";
-        Configuration.baseUrl = "https://demoqa.com";
+
         RestAssured.baseURI = "https://demoqa.com";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
